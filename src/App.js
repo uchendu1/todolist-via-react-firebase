c9269463a96b8037fbc71169380f31e5c8bd8f70
linda <linda@lindas-MacBook-Air.local>
2021-09-25 00:12:17 +0100

    removal of firebase config

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { useState, useEffect } from "react";
-import { collection, onSnapshot } from "@firebase/firestore";
+import { useState } from "react";
+// import { collection, onSnapshot } from "@firebase/firestore";
 
 import "./App.css";
 import { TodoList } from "./components/TodoList";
 import { AppendTask } from "./components/AppendTask";
-import db from "./firebase";
+// import { firestore as db } from "./firebase";
 
-// db.collection("name").add({
+//  db().collection("tasks").add({
 //   name: "linda",
 //   age: 12,
 // });
 
+// const companyDoc = await db().collection(DatabaseKeys.companies.key).doc(companyId).get();
 const App = () => {
-  useEffect(
-    () =>
-      onSnapshot(collection, (db, "tasks"), (snapshot) =>
-      // console.log(snapshot)
-        setList(snapshot.docs.map((doc) => doc.data()))
-      ),
-    []
-  );
+  // useEffect(
+  //   () =>
+  //     onSnapshot(collection, (db, "tasks"), (snapshot) =>
+  //       // console.log(snapshot)
+  //       setList(snapshot.docs.map((doc) => doc.data()))
+  //     ),
+  //   []
+  // );
 
   const dueDate = '<input type="datetime-local" id="task" name="task">';
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
-import { initializeApp } from "firebase/app";
-import * as getFireStore from 'firebase/firestore';
+// import * as firebase from "firebase/app";
+// import 'firebase/firestore';
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCsxih410XyIPA2NgZkV0ejIzJzSgQgQg8",
-  authDomain: "todolist-b2aef.firebaseapp.com",
-  projectId: "todolist-b2aef",
-  storageBucket: "todolist-b2aef.appspot.com",
-  messagingSenderId: "187135324131",
-  appId: "1:187135324131:web:baad8e8d20c14dd1d63ad6"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCsxih410XyIPA2NgZkV0ejIzJzSgQgQg8",
+//   authDomain: "todolist-b2aef.firebaseapp.com",
+//   projectId: "todolist-b2aef",
+//   storageBucket: "todolist-b2aef.appspot.com",
+//   messagingSenderId: "187135324131",
+//   appId: "1:187135324131:web:baad8e8d20c14dd1d63ad6"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default getFireStore(app);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
+// export const { firestore } = firebase;
